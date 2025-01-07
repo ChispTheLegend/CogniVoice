3a3cb242b5df4eb37441d93294e79a7319acde76
Chris Song <92766351+ChispTheLegend@users.noreply.github.com>
2025-01-06 17:34:07 -0800

    Update data_processor.py

--- cognivoice/data_processor.py
+++ cognivoice/data_processor.py
@@ -34,7 +34,7 @@ class TAUKADIALDataset(Dataset):
 
 
         #disvoice = pd.read_parquet('/data/datasets/TAUKADIAL-24/feature/feats_train.parquet')
-        disvoice = pd.read_parquet('/content/drive/MyDrive/TAUKADIAL-24/feature/feats_train.parquet')
+        disvoice = pd.read_parquet('/content/drive/MyDrive/TAUKADIAL-24_feat/train/feats_train.parquet')
         for i in disvoice.columns:
             if i != 'filename':
                 disvoice[i+'_mean'] = disvoice[i].apply(np.mean).fillna(0)
